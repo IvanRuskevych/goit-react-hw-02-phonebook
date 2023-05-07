11fe1c5d41ef3e6fed7900afaebb23dfa22d3d0f
unknown <ivan.ruskevych@gmail.com>
2023-05-07 21:28:30 +0300

    done step 1-3 of hw-02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '+',
+    filter: '',
   };
 
   handleChange = name => e => {
@@ -30,13 +36,23 @@ class App extends Component {
     this.addContact(this.state);
   };
 
+  handleFilterContacts = () => {
+    const { filter, contacts } = this.state;
+    const normilizedFilter = filter.toLowerCase().trim();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normilizedFilter)
+    );
+  };
+
   render() {
+    const filteredContacts = this.handleFilterContacts();
     return (
       <>
         <h2>Phonebook</h2>
         <form autoComplete="off" onSubmit={this.handleSubmit}>
           <label>
-            name
+            Name
             <input
               name="name"
               type="text"
@@ -60,11 +76,21 @@ class App extends Component {
             />
           </label>
           <button type="submit">Add contact</button>
+          <br />
+          <label>
+            Filter
+            <input
+              type="text"
+              name="filter"
+              value={this.state.filter}
+              onChange={this.handleChange('filter')}
+            />
+          </label>
         </form>
 
         <h2>Contacts</h2>
         <ul>
-          {this.state.contacts.map(contact => {
+          {filteredContacts.map(contact => {
             return (
               <li key={nanoid(5)}>{`${contact.name}: ${contact.number}`}</li>
             );
