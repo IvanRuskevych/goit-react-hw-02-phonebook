56b02822b8e0e9ffae891cec539a3a663a9e21cd
unknown <ivan.ruskevych@gmail.com>
2023-05-08 01:53:28 +0300

    done styling

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
+import TextField from '@mui/material/TextField';
+import css from './Filter.module.css';
 
 function Filter({ filterValue, onChange }) {
   return (
-    <label>
+    <label className={css.filter}>
       Find contacts by name
-      <input
+      <TextField
+        id="standard-basic"
+        label="Print name ..."
+        variant="standard"
         type="text"
         name="filter"
         value={filterValue}
