0f9af90f43768469cda5c33625f10c8c967fc82d
unknown <ivan.ruskevych@gmail.com>
2023-05-07 19:46:40 +0300

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Field, Form, Formik } from 'formik';
+import React from 'react';
+
+const initialValues = {
+  contacts: [],
+  name: '',
+  number: '+',
 };
+
+export default function App() {
+  const handleSubmit = (values, actions) => {
+    console.log(initialValues.contacts);
+    console.log(values);
+    console.log(actions);
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="off">
+        <label htmlFor="name">
+          name
+          <Field
+            name="name"
+            type="text"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <Field
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
+
+  // ----------------------
+}
